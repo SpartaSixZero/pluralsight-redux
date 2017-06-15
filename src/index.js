2daa9dea6531d366708cd5b7df6296168e28b686
ms030441 <mark.sun@cerner.com>
2017-06-15 08:05:25 -0400

    Fixed npm build

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
+/*eslint-disable import/default*/
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 import configureStore from './store/configureStore';
-/* eslint-disable import/default */
 import {Provider} from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
